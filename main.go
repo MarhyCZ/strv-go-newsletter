ef7b43bc0280d64128adeb5f46b8bf84350576cb
Matěj Oliva <matej.oliva@outlook.com>
2023-05-19 05:13:08 +0200

    [FEATURE] Firabase init

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
-package strv_go_newsletter
+package main
 
 import (
+	/* "fmt"
+
 	"github.com/marhycz/strv-go-newsletter/database"
-	"github.com/marhycz/strv-go-newsletter/environment"
+	"github.com/marhycz/strv-go-newsletter/environment" */
+
+	initFirebase "github.com/marhycz/strv-go-newsletter/database/firebase"
 )
 
 func main() {
-	env := &environment.Env{
+	/* env := &environment.Env{
 		Database: database.NewConnection(),
 	}
+	fmt.Println(env) */
 	// api.Serve(env)
+	initFirebase.InitSDK()
 }
