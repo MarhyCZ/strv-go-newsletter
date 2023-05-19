9cca5485cc554862c42a16d30b664c1694f65754
Michal Marhan <marhanmichal@gmail.com>
2023-05-19 22:34:11 +0200

    Use config folder to store additional env files

--- repository/firebase/initSDK.go
+++ repository/firebase/initSDK.go
@@ -11,53 +11,53 @@ import (
 )
 
 // Use a service account
-func InitSDK(){
-  ctx := context.Background()
-  sa := option.WithCredentialsFile("firebase_key.json")
-  app, err := firebase.NewApp(ctx, nil, sa)
-  if err != nil {
-    log.Fatalln(err)
-  }
-  
-  client, err := app.Firestore(ctx)
-  if err != nil {
-    log.Fatalln(err)
-  }
+func InitSDK() {
+	ctx := context.Background()
+	sa := option.WithCredentialsFile("config/firebase_key.json")
+	app, err := firebase.NewApp(ctx, nil, sa)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-  defer client.Close()
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-  //get all subscriptions
-  iter := client.Collection("subscription").Documents(ctx)
-  for {
-    doc, err := iter.Next()
-    if err == iterator.Done {
-      break
-    }
-    if err != nil {
-      log.Fatalf("Failed to iterate: %v", err)
-    }
-    fmt.Println(doc.Data())
-  }
-  //get subscription
-  sub := client.Collection("subscription").Where("id", "==", "csBgld5GN7hn4lB0ISaX").Documents(ctx)
-  for {
-    doc, err := sub.Next()
-    if err == iterator.Done {
-      break
-    }
-    if err != nil {
-      log.Fatalf("Failed to iterate: %v", err)
-    }
-    fmt.Println(doc.Data())
-  }
+	defer client.Close()
 
-  //new subscription TO FIX
-  /* _, _, err := client.Collection("subscription").Add(ctx, map[string]interface{}{
-    "email": "Jack@McMc.cz",
-    "id": "239kjl21kj312ljk232",
-    "newsletter_id": "13",
-  })
-  if err != nil {
-    log.Fatalf("Failed adding alovelace: %v", err)
-  } */
+	//get all subscriptions
+	iter := client.Collection("subscription").Documents(ctx)
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Failed to iterate: %v", err)
+		}
+		fmt.Println(doc.Data())
+	}
+	//get subscription
+	sub := client.Collection("subscription").Where("id", "==", "csBgld5GN7hn4lB0ISaX").Documents(ctx)
+	for {
+		doc, err := sub.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Failed to iterate: %v", err)
+		}
+		fmt.Println(doc.Data())
+	}
+
+	//new subscription TO FIX
+	/* _, _, err := client.Collection("subscription").Add(ctx, map[string]interface{}{
+	  "email": "Jack@McMc.cz",
+	  "id": "239kjl21kj312ljk232",
+	  "newsletter_id": "13",
+	})
+	if err != nil {
+	  log.Fatalf("Failed adding alovelace: %v", err)
+	} */
 }
