f8d61d9903fe7053947b1a1269fc05a6d02139c3
eMkoC4 <ivansvoboda9gmail.com>
2023-05-21 17:32:44 +0200

    Added logout

--- api/editor.go
+++ api/editor.go
@@ -7,7 +7,6 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/golang-jwt/jwt/v4"
 	"github.com/marhycz/strv-go-newsletter/repository/database"
 
 	"github.com/go-chi/chi/v5"
@@ -29,6 +28,9 @@ func (rest *Rest) routeEditor(r *chi.Mux) {
 	})
 	r.Route("/resetpassword", func(r chi.Router) {
 		r.Post("/", rest.resetPassword) // POST /editor
+	})
+	r.Route("/logout", func(r chi.Router) {
+		r.Get("/", rest.logout) // POST /editor
 	})
 	r.Route("/getEditors", func(r chi.Router) {
 		r.Get("/", rest.getEditors) // POST /editor
@@ -68,22 +70,9 @@ func (rest *Rest) signup(w http.ResponseWriter, r *http.Request) {
 }
 
 func (rest *Rest) getEditors(w http.ResponseWriter, r *http.Request) {
-
-	c, err := r.Cookie("token")
-	if err != nil {
-		if err == http.ErrNoCookie {
-			w.WriteHeader(http.StatusUnauthorized)
-			w.Write([]byte(strconv.Itoa(http.StatusUnauthorized) + ": " + http.StatusText(http.StatusUnauthorized)))
-			return
-		}
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(strconv.Itoa(http.StatusBadRequest) + ": " + http.StatusText(http.StatusBadRequest)))
-		return
-	}
-
-	if code, err := Auth(c); err != nil {
-		w.WriteHeader(code)
-		w.Write([]byte(strconv.Itoa(code) + ": " + http.StatusText(code)))
+	if c, err := AuthToken(r); err != nil {
+		w.WriteHeader(c)
+		w.Write([]byte(strconv.Itoa(c) + ": " + http.StatusText(c)))
 		return
 	}
 
@@ -116,43 +105,38 @@ func (rest *Rest) login(w http.ResponseWriter, r *http.Request) {
 	}
 	if !CheckPasswordHash(LoginInput.Password, e.Password) {
 		w.WriteHeader(http.StatusUnauthorized)
-		w.Write([]byte("wrong login credentials"))
+		w.Write([]byte(strconv.Itoa(http.StatusUnauthorized) + ": " + http.StatusText(http.StatusUnauthorized)))
+		w.Write([]byte("\n" + "Wrong login credentials"))
 		return
 	}
 
-	expirationTime := time.Now().Add(30 * time.Minute)
-	// Create the JWT claims, which includes the username and expiry time
-	claims := &Claims{
-		Username: LoginInput.Email,
-		RegisteredClaims: jwt.RegisteredClaims{
-			// In JWT, the expiry time is expressed as unix milliseconds
-			ExpiresAt: jwt.NewNumericDate(expirationTime),
-		},
-	}
-
-	// Declare the token with the algorithm used for signing, and the claims
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-
-	// Create the JWT string
-	tokenString, err := token.SignedString(jwtKey)
+	token, expDate, err := CreateNewJWT(LoginInput.Email)
 	if err != nil {
-		// If there is an error in creating the JWT return an internal server error
 		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(strconv.Itoa(http.StatusInternalServerError) + ": " + http.StatusText(http.StatusInternalServerError)))
 		return
 	}
 
-	// Finally, we set the client cookie for "token" as the JWT we just generated
-	// we also set an expiry time which is the same as the token itself
 	http.SetCookie(w, &http.Cookie{
 		Name:    "token",
-		Value:   tokenString,
-		Expires: expirationTime,
+		Value:   token,
+		Expires: expDate,
 	})
 
-	w.Write([]byte(tokenString))
+	w.Write([]byte("JTW token: " + token))
 
 }
 
 func (rest *Rest) resetPassword(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("reset email sent succesfully"))
+}
+
+func (rest *Rest) logout(w http.ResponseWriter, r *http.Request) {
+	// immediately clear the token cookie
+	http.SetCookie(w, &http.Cookie{
+		Name:    "token",
+		Expires: time.Now(),
+	})
+	w.WriteHeader(http.StatusAccepted)
+	w.Write([]byte("logout successful"))
 }
--- api/util.go
+++ api/util.go
@@ -3,6 +3,7 @@ package api
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/golang-jwt/jwt/v4"
@@ -38,9 +39,16 @@ func CheckPasswordHash(password, hash string) bool {
 	return err == nil
 }
 
-func Auth(c *http.Cookie) (int, error) {
-	tknStr := c.Value
+func AuthToken(r *http.Request) (int, error) {
+	c, err := r.Cookie("token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			return http.StatusUnauthorized, err
+		}
+		return http.StatusBadRequest, err
+	}
 
+	tknStr := c.Value
 	claims := &Claims{}
 
 	tkn, err := jwt.ParseWithClaims(tknStr, claims, func(token *jwt.Token) (interface{}, error) {
@@ -56,4 +64,26 @@ func Auth(c *http.Cookie) (int, error) {
 		return http.StatusBadRequest, err
 	}
 	return http.StatusAccepted, nil
+}
+
+func CreateNewJWT(email string) (string, time.Time, error) {
+
+	//30 minutes
+	expirationTime := time.Now().Add(30 * time.Minute)
+
+	claims := &Claims{
+		Username: email,
+		RegisteredClaims: jwt.RegisteredClaims{
+			// In JWT, the expiry time is expressed as unix milliseconds
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		return "", expirationTime, err
+	}
+
+	return tokenString, expirationTime, nil
 }
